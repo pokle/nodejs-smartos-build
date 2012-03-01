680b21e466ff6444e91867cd9dcb0e31b4a90e4c
Super-User <tushar.pokle@gmail.com>
2012-03-01 14:08:58 +0000

    VER

--- build-redis.sh
+++ build-redis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-REDIS=2.4.8
+VER=2.4.8
 
 mkdir -p tmp
 cd tmp
-test -d redis-$REDIS || curl http://redis.googlecode.com/files/redis-$REDIS.tar.gz | tar xz
-cd redis-$REDIS
+test -d redis-$VER || curl http://redis.googlecode.com/files/redis-$VER.tar.gz | tar xz
+cd redis-$VER
 test -x src/redis-server || make
 make install
 
