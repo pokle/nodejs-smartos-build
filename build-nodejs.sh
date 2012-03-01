587eaf4be9b7b5b5bc5afd3820ffb50a9665d231
Super-User <tushar.pokle@gmail.com>
2012-03-01 14:03:59 +0000

    nodejs + test before doing

---
+++ build-nodejs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VER=0.6.11
+
+mkdir -p tmp
+cd tmp
+test -d node-v$VER || curl http://nodejs.org/dist/v$VER/node-v$VER.tar.gz | tar xz
+cd node-v$VER
+
+test -x out/Release/node || (./configure; make)
+
--- build-redis.sh
+++ build-redis.sh
@@ -4,9 +4,9 @@ REDIS=2.4.8
 
 mkdir -p tmp
 cd tmp
-curl http://redis.googlecode.com/files/redis-$REDIS.tar.gz | tar xz
+test -d redis-$REDIS || curl http://redis.googlecode.com/files/redis-$REDIS.tar.gz | tar xz
 cd redis-$REDIS
-make
+test -x src/redis-server || make
 make install
 
 
